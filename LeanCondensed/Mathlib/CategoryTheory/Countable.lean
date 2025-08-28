0891186bdeed2500fa0575fdadef9cbc072d6ddb
Jonas van der Schaaf <niet@jonasvanderschaaf.nl>
2025-08-28 10:41:44 +0200

    Formatting

--- LeanCondensed/Mathlib/CategoryTheory/Countable.lean
+++ LeanCondensed/Mathlib/CategoryTheory/Countable.lean
@@ -12,8 +12,7 @@ noncomputable section
 open CategoryTheory Quiver Countable
 
 instance {J : Type u} [Countable J] [Category J] [IsThin J] :
-    CountableCategory J :=
-  CountableCategory.mk inferInstance inferInstance
+    CountableCategory J := CountableCategory.mk inferInstance inferInstance
 
 noncomputable instance {J : Type u} [Finite J] [Category J] [IsThin J] : FinCategory J := by
   apply FinCategory.mk (Fintype.ofFinite _) (fun j j' ↦ Fintype.ofFinite _)
